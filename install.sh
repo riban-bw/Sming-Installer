a588ec930fb0b0ab0d5ac0054a5f6237be498c9b
U-NATIONAL\waltob02 <waltob02@B1-L0S094884.national.core.bbc.co.uk>
2018-01-27 13:44:18 +0000

    Adds command line parser. Adds ability to select which modules to install. Checks if modules already installed and offers Backup/Delete/Skip/Exit. Silent and verbose output options added. Force unzip to overwrite existing installation if exists.

--- install.sh
+++ install.sh
@@ -9,8 +9,176 @@
 # install.sh
 #
 
+# Debug output
+# param Debug level (1=Normal 2=Debug)
+# param Text to print
+Debug()
+{
+	if [ $SILENT -eq 1 ]
+	then
+		return
+	elif [ $VERBOSE -eq 0 -a  "$1" = "2" ]
+	then
+		return
+	fi
+	echo "$2"
+}
+
+# Check if a module is already installed
+# Offer user choice to backup, delete, exit or skip instalation step
+# param Module name
+# param Module relative installation path
+CheckModule()
+{
+	module=$1
+	path=$2
+	RESPONSE=$FORCE
+	if [ ${!1} -eq 1 ]
+	then
+		if [ -e $path ]
+		then
+			if [ "$FORCE" = "A" ]
+			then
+				echo "$1 appears to be installed at ./$path. Backup, Delete, Exit or Skip? [b/d/e/S]"
+				read RESPONSE
+			fi
+			if [ "$RESPONSE" = "b" ]
+			then
+			  Debug 1 "Backing up $path to $path.bu"
+			  mv -f $path $path.bu
+			elif [ "$RESPONSE" = "d" ]
+			then
+			  rm -r $path
+			elif [ "$RESPONSE" = "e" ]
+			then
+			  exit 0
+			else
+			  eval "$1=0"
+			fi
+		fi
+	fi
+}
+
+# Initalise variables
+FORCE=A
+SMING=0
+SDK=0
+ESPTOOL=0
+XTENSA=0
+SPIFFY=0
+ALL=1
+VERBOSE=0
+SILENT=0
+
+# Parse command line
+while getopts ":hVSdbsi:" opt; do
+  case ${opt} in
+    [h,\?]) # Help, Invalid option
+	  echo "Usage: $0 [options]"
+	  echo 
+	  echo "  Options:"
+	  echo "    -h             Show this help"
+	  echo "    -V             Verbose output"
+	  echo "    -S             Silent - no output"
+	  echo "    -d             Delete any modules already installed - no questions"
+	  echo "    -b             Backup any modules already installed - no questions"
+	  echo "    -s             Skip any modules already installed - no questions"
+	  echo "    -i <module>    Install <module>"
+	  echo "      Modules:"
+	  echo "        sming      SMING framework"
+	  echo "        sdk        ESP8266 SDK"
+	  echo "        esptool    esptool.py"
+	  echo "        xtensa     XTENSA c/c++ compiler"
+	  echo "        spiffy     SPIFFS file system tool"
+	  echo "        all        Install all modules (default)"
+	  echo "      Multiple -i options may be provided. Default is to install all modules."
+	  echo "      If -i is provided then only the specified modules will be installed."
+      exit 0
+      ;;
+    V ) # Verbose
+	  VERBOSE=1
+      ;;
+    S ) # Silent
+	  SILENT=1
+      ;;
+    d ) # Delete existing installed modules
+	  FORCE=d
+      ;;
+	b ) # Backup existing installed modules
+	  FORCE=b
+	  ;;
+	s ) # Skip existing installed modules
+	  FORCE=s
+	  ;;
+	i ) # Install module
+		case ${OPTARG} in
+		  sming )
+		    SMING=1
+			ALL=0
+		    ;;
+		  skd )
+		    SDK=1
+			ALL=0
+            ;;
+          esptool )
+		    ESPTOOL=1
+			ALL=0
+            ;;
+          xtensa )
+		    XTENSA=1
+			ALL=0
+		    ;;
+          spiffy )
+		    SPIFFY=1
+			ALL=0
+		    ;;
+          all )
+		    ALL=1
+		    ;;
+		  * ) # Wrong argument provided
+		    echo "Incorrect argument for -i. $0 -h for help"
+			exit 1
+			;;
+		esac
+	  ;;
+	: )
+	  echo "Invalid option: $OPTARG requires an argument. $0 -h for help"
+	  exit 1
+	  ;;
+  esac
+done
+
+##START OF MAIN CODE##
+if [ $ALL -eq 1 ]
+then
+  SMING=1
+  SDK=1
+  ESPTOOL=1
+  XTENSA=1
+  SPIFFY=1
+fi
+
+if [ $SMING -eq 1 ]; then CheckModule SMING Sming/Sming;fi
+if [ $SDK -eq 1 ]; then CheckModule SDK Sming/esp-toolkit/sdk;fi
+if [ $ESPTOOL -eq 1 ]; then CheckModule ESPTOOL Sming/esp-toolkit/esptool;fi
+if [ $XTENSA -eq 1 ]; then CheckModule XTENSA Sming/esp-toolkit/xtensa-lx106-elf;fi
+if [ $SPIFFY -eq 1 ]; then CheckModule SPIFFY Sming/Sming/spiffy/spiffy;fi
+
+Debug 2 "SMING:   $SMING"
+Debug 2 "SDK:     $SDK"
+Debug 2 "ESPTOOL: $ESPTOOL"
+Debug 2 "XTENSA:  $XTENSA"
+Debug 2 "SPIFFY:  $SPIFFY"
+
+INSTALL=$(($SMING + $SDK + $ESPTOOL + $XTENSA + $SPIFFY))
+if [ $INSTALL -eq 0 ]
+then
+  Debug 1 "Nothing selected to install"
+  exit 0
+fi
+
 # Check required tools are installed
-echo "Checking required tools are installed..."
+Debug 1 "Checking required tools are installed..."
 MISSING=""
 for app in mkdir wget rm uname make g++ python
 do
@@ -19,7 +187,7 @@ done
 unzip -v &>/dev/null || MISSING+="unzip "
 if [ "$MISSING" != "" ]
 then
-  echo "Please install $MISSING then re-run installer."
+  Debug 1 "Please install $MISSING then re-run installer."
   exit 1
 fi
 
@@ -36,63 +204,67 @@ else
   WGET="wget -q --show-progress"
 fi
 
-UNZIP="unzip -q"
-
-# Check if Sming folder exists
-# TODO: Avoid deleting whole Sming folder in case user keeps other data there
-if [ -d Sming ]
-then
-  echo "Sming folder already exists. Delete and continue or Exit? [d/E]:"
-  read RESPONSE
-  if [ "$RESPONSE" = "d" ]
-  then
-    echo "Deleting `pwd`/Sming..."
-    rm -r Sming 2> /dev/null
-  else
-    exit 1
-  fi
-fi
-if [ -d Sming ]
-then
-  echo "Failed to delete Sming folder. Please manually remove before installing."
-  exit 1
-fi
+UNZIP="unzip -oq"
 
 # Download the platform specific cross compiler
 PLATFORM=`uname -sm`
-echo "Downloading Sming packages for $PLATFORM..."
-if [ "$PLATFORM" = "Linux armv6l" ]
+if [ $XTENSA -eq 1 ]
 then
-  $WGET -O $TEMP/xtensa-lx106-elf.zip https://www.dropbox.com/s/c3f718jiqzh1cvo/xtensa-lx106-elf-linux-armv6l.zip
-elif [[ "$PLATFORM" == "Linux i686" ]]
-then
-  $WGET -O $TEMP/xtensa-lx106-elf.zip https://www.dropbox.com/s/c9elu7pjdp87jr5/xtensa-lx106-elf-linux-i686.zip
-elif [[ "$PLATFORM" == "Linux x86_64" ]]
-then
-  $WGET -O $TEMP/xtensa-lx106-elf.zip https://www.dropbox.com/s/d2u56kqfbm4twgi/xtensa-lx106-elf-linux-x86_64.zip
-elif [[ "$PLATFORM" == "CYGWIN_NT"*"WOW i686" ]]
-then
-  $WGET -O $TEMP/xtensa-lx106-elf.zip https://www.dropbox.com/s/1cn969exsnp88ls/xtensa-lx106-elf-cygwin32-i686.zip
-elif [[ "$PLATFORM" == "Darwin x86"* ]]
-then
-  $WGET -O $TEMP/xtensa-lx106-elf.zip https://www.dropbox.com/s/8q9g22di7al1tea/xtensa-lx106-elf-osx-x86_64.zip
-else
-  echo "Unsupported platform $PLATFORM"
-  exit 1
+	Debug 1 "Downloading Sming packages for $PLATFORM..."
+	if [ "$PLATFORM" = "Linux armv6l" ]
+	then
+	  $WGET -O $TEMP/xtensa-lx106-elf.zip https://www.dropbox.com/s/c3f718jiqzh1cvo/xtensa-lx106-elf-linux-armv6l.zip
+	elif [[ "$PLATFORM" == "Linux i686" ]]
+	then
+	  $WGET -O $TEMP/xtensa-lx106-elf.zip https://www.dropbox.com/s/c9elu7pjdp87jr5/xtensa-lx106-elf-linux-i686.zip
+	elif [[ "$PLATFORM" == "Linux x86_64" ]]
+	then
+	  $WGET -O $TEMP/xtensa-lx106-elf.zip https://www.dropbox.com/s/d2u56kqfbm4twgi/xtensa-lx106-elf-linux-x86_64.zip
+	elif [[ "$PLATFORM" == "CYGWIN_NT"*"WOW i686" ]]
+	then
+	  $WGET -O $TEMP/xtensa-lx106-elf.zip https://www.dropbox.com/s/1cn969exsnp88ls/xtensa-lx106-elf-cygwin32-i686.zip
+	elif [[ "$PLATFORM" == "Darwin x86"* ]]
+	then
+	  $WGET -O $TEMP/xtensa-lx106-elf.zip https://www.dropbox.com/s/8q9g22di7al1tea/xtensa-lx106-elf-osx-x86_64.zip
+	else
+	  Debug 1 "Unsupported platform $PLATFORM"
+	  exit 1
+	fi
 fi
 
 # Download the platform agnostic packages
-$WGET -O $TEMP/Sming.zip https://www.dropbox.com/s/k8zwqo114bj213d/Sming-3.5.0_20171231.zip
-$WGET -O $TEMP/SDK.zip https://www.dropbox.com/s/b8yjilq9a6xagdm/ESP8266_NONOS_SDK_V2.0.0_16_08_10.zip
-#$WGET -O $TEMP/SDK.zip https://www.dropbox.com/s/3h9f3x236qb8ds5/ESP8266_NONOS_SDK-2.1.0.zip
-$WGET -O $TEMP/esptool.zip https://www.dropbox.com/s/21ceaa8u9254k1f/esptool.zip
+if [ $SMING -eq 1 ]
+then
+	$WGET -O $TEMP/Sming.zip https://www.dropbox.com/s/k8zwqo114bj213d/Sming-3.5.0_20171231.zip
+fi
+if [ $SDK -eq 1 ]
+then
+  $WGET -O $TEMP/SDK.zip https://www.dropbox.com/s/b8yjilq9a6xagdm/ESP8266_NONOS_SDK_V2.0.0_16_08_10.zip
+  #$WGET -O $TEMP/SDK.zip https://www.dropbox.com/s/3h9f3x236qb8ds5/ESP8266_NONOS_SDK-2.1.0.zip
+fi
+if [ $ESPTOOL -eq 1 ]
+then
+  $WGET -O $TEMP/esptool.zip https://www.dropbox.com/s/21ceaa8u9254k1f/esptool.zip
+fi
 
 # Install the packages
-echo "Installing Sming to `pwd`/Sming..."
-$UNZIP $TEMP/Sming.zip && echo "Sming installed"
-$UNZIP -d Sming/esp-toolkit $TEMP/SDK.zip && echo "SDK installed"
-$UNZIP -d Sming/esp-toolkit $TEMP/esptool.zip && echo "esptool installed"
-$UNZIP -d Sming/esp-toolkit $TEMP/xtensa-lx106-elf.zip && echo "xtensa-lx106-elf compiler installed"
+Debug 1 "Installing Sming to `pwd`/Sming..."
+if [ $SMING -eq 1 ]
+then
+  $UNZIP $TEMP/Sming.zip && Debug 1 "Sming installed"
+fi
+if [ $SDK -eq 1 ]
+then
+  $UNZIP -d Sming/esp-toolkit $TEMP/SDK.zip && Debug 1 "SDK installed"
+fi
+if [ $ESPTOOL -eq 1 ]
+then
+  $UNZIP -d Sming/esp-toolkit $TEMP/esptool.zip && Debug 1 "esptool installed"
+fi
+if [ $ESPTOOL -eq 1 ]
+then
+  $UNZIP -d Sming/esp-toolkit $TEMP/xtensa-lx106-elf.zip && Debug 1 "xtensa-lx106-elf compiler installed"
+fi
 
 # TODO: Remove downloaded packages
 
@@ -103,20 +275,23 @@ chmod 755 Sming/setenv
 
 . Sming/setenv
 
-# Build spiffy
-echo "Building the SPIFF command line tool, spiffy..."
-make -C $SMING_HOME/spiffy clean spiffy &>/dev/null
+if [ $SPIFFY -eq 1 ]
+then
+  # Build spiffy
+  Debug 1 "Building the SPIFF command line tool, spiffy..."
+  make -C $SMING_HOME/spiffy clean spiffy &>/dev/null
+fi
 
 # Test installation
-echo "Testing the installation..."
+Debug 1 "Testing the installation..."
 make -C $SMING_HOME test &>$TEMP/test_results.txt
 if [ $? -ne 0 ]
 then
-  echo "[ERROR] Tests have failed. Please report an issue to https://github.com/riban-bw/Sming-Release/issues with the following information:"
-  echo "==========================================="
-  echo "UNAME: `uname -a`"
+  Debug 1 "[ERROR] Tests have failed. Please report an issue to https://github.com/riban-bw/Sming-Release/issues with the following information:"
+  Debug 1 "==========================================="
+  Debug 1 "UNAME: `uname -a`"
   cat $TEMP/test_results.txt
-  echo "==========================================="
+  Debug 1 "==========================================="
   exit 1
 fi
 
@@ -124,19 +299,19 @@ fi
 rm -r $TEMP
 
 # Final confirmation
-echo " "
-echo "Sming and support tools now installed."
-echo "There are sample projects in $SMING_HOME../samples."
-echo "To get started:"
-echo "  " `pwd` "/set_env"
-echo "   mkdir -p ~/SmingProjects/HelloWorld"
-echo "   cd ~/SmingProjects/HelloWorld"
-echo "   cp -r $SMING_HOME../samples/Basic_Blink/* ."
-echo "   edit HelloWorld/Makefile-user.mk to set serial port, etc."
-echo "   edit HelloWorld/app/application.cpp with your source code"
-echo "   make"
-echo " "
-echo "To upload project to the ESP8266"
-echo "   Connect ESP8266 module to serial port"
-echo "   Ensure COM_PORT is set to a valid serial device in Makefile-user"
-echo "   make flash"
+Debug 1 " "
+Debug 1 "Sming and support tools now installed."
+Debug 1 "There are sample projects in $SMING_HOME../samples."
+Debug 1 "To get started:"
+Debug 1 "  " `pwd` "/set_env"
+Debug 1 "   mkdir -p ~/SmingProjects/HelloWorld"
+Debug 1 "   cd ~/SmingProjects/HelloWorld"
+Debug 1 "   cp -r $SMING_HOME../samples/Basic_Blink/* ."
+Debug 1 "   edit HelloWorld/Makefile-user.mk to set serial port, etc."
+Debug 1 "   edit HelloWorld/app/application.cpp with your source code"
+Debug 1 "   make"
+Debug 1 " "
+Debug 1 "To upload project to the ESP8266"
+Debug 1 "   Connect ESP8266 module to serial port"
+Debug 1 "   Ensure COM_PORT is set to a valid serial device in Makefile-user"
+Debug 1 "   make flash"
